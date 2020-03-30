9cf22bf52bd203a92edcdf24b3097482332f21e3
andrepelealu <andre02.9d@gmail.com>
2020-03-30 21:06:51 +0700

    add credit footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import ContainerComponent from './components/Container'
 import Result from './components/Result'
 import Assessment from './components/Assessment' 
+import Footer from './components/Footer'
 import {Route,Switch} from 'react-router-dom'
 import { Container } from 'reactstrap';
 
@@ -19,9 +20,10 @@ function App() {
       
       <Route exact path="/test-corona" component={Assessment}/>
       <Route exact path="/hasil" component={Result}/>
-      
 
       </Switch>
+      <Footer/>
+
     </Container>
     
     </>
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <br/>
+            <div class="row">
+                <div class="col-sm-12 text-center">
+             
+                        <h5>Made with &hearts; by <a href="https://andrepelealu.com">AndrePelealu</a> </h5>
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
